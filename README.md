1b7a3688d867dbda3b42c3c053f771257297026d
ankera <15189120919@163.com>
2024-08-19 23:27:27 +0800

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+### spring-ssm-01-po
---
+++ src/test/java/com/ankers/SpringSsm01PoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsm01PoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
